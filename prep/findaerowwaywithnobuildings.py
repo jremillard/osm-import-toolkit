e33a13765cca7e42f8977b17f0152bacda8023ab
jremillard <remillard.jason@gmail.com>
2013-05-26 19:18:28 -0400

    make pre/find* scripts have a help option, db name option, and more robust to different enviroments

--- prep/findaerowwaywithnobuildings.py
+++ prep/findaerowwaywithnobuildings.py
@@ -1,14 +1,99 @@
-#!usr/bin/python
+#!/usr/bin/python
 
 # Jason Remillard - This file is in the public domain.
 
-import sys, os, zipfile
+import os
+import sys
+import tempfile
+import optparse
+import subprocess
+
+# run a command, return true if it worked, false if it did not work, used 
+# to see if something is installed. Insures nothing goes to stdout,stderr
+# used to make sure the environment is setup.
+def commandCheck( args ) :
+  ret = 1
+  try: 
+    subprocess.check_output(args,stderr=subprocess.STDOUT)
+  except:
+    ret = 0
+  return ret
+ 
+parser = optparse.OptionParser(
+   description='Finds all aeroway = \'terminal\' ways without a building=* way. '
+               'The dependencies are ogr2ogr, a postGIS database loaded with OSM '
+               'data using with osm2pgsql, and ogr2osm. ogr2osm must be in the '
+               'current directory, on path, or in ./ogr2osm directory. ',
+   usage='findareowaywithnobuildings.py [options] [outputfile.osm]')
+
+parser.add_option('','--dbname',help='PostGIS database name, defaults to gis.',dest='database',default='gis')
+
+(param,filenames) = parser.parse_args()
+
+outputFile = 'badaeroways.osm'
+if ( len ( filenames) > 0 ) :
+  outputFile = filenames[0]
+
+# see if ogr2ogr is installed on path.
+if ( commandCheck(["ogr2ogr","--version"]) == False ) : 
+  print("error: ogr2ogr is not installed or not on the path.")
+  sys.exit(1)
+
+# try to find ogr2osm.py, not packaged yet by anybody.
+ogr2osmCmd = ""
+if ( commandCheck(["./ogr2osm/ogr2osm.py","-h"])) : 
+  ogr2osmCmd = "./ogr2osm/ogr2osm.py"
+elif ( commandCheck(["python","ogr2osm/ogr2osm.py","-h"])) : 
+  ogr2osmCmd = "python ogr2osm/ogr2osm.py"
+elif ( commandCheck(["./ogr2osm.py","-h"])) : 
+  ogr2osmCmd = "./ogr2osm.py -h"
+elif ( commandCheck(["python","ogr2osm.py","-h"])) : 
+  ogr2osmCmd = "python ogr2osm.py"
+elif ( commandCheck(["ogr2osm","-h"])) : 
+  ogr2osmCmd = "ogr2osm"
+else :
+  print("error: ogr2osm is not installed or is not on the path.")
+  sys.exit(1)
+
+if ( os.path.isfile(outputFile)) :
+  os.remove(outputFile)
 
 # get aeroway=terminals without building tags.
-sql = "select way from planet_osm_polygon as way1 where way1.aeroway = 'terminal' and not exists (select * from planet_osm_polygon as way2 where way2.building != '' and ST_Intersects(way1.way,way2.way))"
+sql = ('select way ' 
+       'from planet_osm_polygon as way1 '
+       'where '
+       '  way1.aeroway = \'terminal\' and '
+       'not exists '
+       '  (select * '
+       '   from planet_osm_polygon as way2 '
+       '   where way2.building != \'\' and ST_Intersects(way1.way,way2.way) '
+       '  )')
 
-os.system("ogr2ogr -sql \"" + sql + "\" -overwrite -f 'ESRI Shapefile' temp/badaeroway.shp PG:dbname=gis " )
-os.system("python ogr2osm/ogr2osm.py -f -o output/badaeroway.osm temp/badaeroway.shp")
+tempShapeFile = tempfile.mktemp();
+
+if ( os.system("ogr2ogr -sql \"" + sql + "\" -overwrite -f 'ESRI Shapefile' " + tempShapeFile + ".shp PG:dbname=\"" + param.database + "\" ") ) :
+  print("error: ogr2ogr command failed.")
+  sys.exit(1)
+
+if ( os.system(ogr2osmCmd + " -f -o " + outputFile + " " + tempShapeFile + ".shp") ) :
+  print("error: ogr2osm command failed.")
+  os.remove(tempShapeFile + ".shp")
+  os.remove(tempShapeFile + ".dbf")
+  os.remove(tempShapeFile + ".shx")
+  sys.exit(1)
+
+os.remove(tempShapeFile + ".shp")
+os.remove(tempShapeFile + ".dbf")
+os.remove(tempShapeFile + ".shx")
+
+print("Writing output file " + outputFile)
+
+sys.exit(0)
+
+
+
+
+  
 
 
 
--- prep/findoverlappingbuildings.py
+++ prep/findoverlappingbuildings.py
@@ -1,12 +1,63 @@
-#!usr/bin/python
+#!/usr/bin/python
 
 # Jason Remillard - This file is in the public domain.
 
-import sys, os, zipfile
+import os
+import sys
+import tempfile
+import optparse
+import subprocess
 
-os.system("rm temp/*")
+# run a command, return true if it worked, false if it did not work, used 
+# to see if something is installed. Insures nothing goes to stdout,stderr
+# used to make sure the environment is setup.
+def commandCheck( args ) :
+  ret = 1
+  try: 
+    subprocess.check_output(args,stderr=subprocess.STDOUT)
+  except:
+    ret = 0
+  return ret
+ 
+parser = optparse.OptionParser(
+   description='Creates an .osm file containing overlapping buildings > 1 sq meter. '
+               'The dependencies are ogr2ogr, a postGIS database loaded with OSM '
+               'data using with osm2pgsql, and ogr2osm. ogr2osm must be in the '
+               'current directory, on path, or in ./ogr2osm directory. ',
+   usage='findoverlappingbuildings.py [options] [outputfile.osm]')
 
-# get structures missing from OSM
+parser.add_option('','--dbname',help='PostGIS database name, defaults to gis.',dest='database',default='gis')
+
+(param,filenames) = parser.parse_args()
+
+outputFile = 'overlappingbuildings.osm'
+if ( len ( filenames) > 0 ) :
+  outputFile = filenames[0]
+
+# see if ogr2ogr is installed on path.
+if ( commandCheck(["ogr2ogr","--version"]) == False ) : 
+  print("error: ogr2ogr is not installed or not on the path.")
+  sys.exit(1)
+
+# try to find ogr2osm.py, not packaged yet by anybody.
+ogr2osmCmd = ""
+if ( commandCheck(["./ogr2osm/ogr2osm.py","-h"])) : 
+  ogr2osmCmd = "./ogr2osm/ogr2osm.py"
+elif ( commandCheck(["python","ogr2osm/ogr2osm.py","-h"])) : 
+  ogr2osmCmd = "python ogr2osm/ogr2osm.py"
+elif ( commandCheck(["./ogr2osm.py","-h"])) : 
+  ogr2osmCmd = "./ogr2osm.py -h"
+elif ( commandCheck(["python","ogr2osm.py","-h"])) : 
+  ogr2osmCmd = "python ogr2osm.py"
+elif ( commandCheck(["ogr2osm","-h"])) : 
+  ogr2osmCmd = "ogr2osm"
+else :
+  print("error: ogr2osm is not installed or is not on the path.")
+  sys.exit(1)
+
+if ( os.path.isfile(outputFile)) :
+  os.remove(outputFile)
+
 sql = ("select way "
        "from planet_osm_polygon as way1 "
        "where "
@@ -22,12 +73,26 @@ sql = ("select way "
        "   ST_IsValid( way2.way) and "
        "   ST_Area(ST_Intersection(way1.way, way2.way)) > 1 )")
 
-print sql
+tempShapeFile = tempfile.mktemp();
 
-os.system("ogr2ogr -sql \"" + sql + "\" -overwrite -f 'ESRI Shapefile' temp/overlappingbuildings.shp PG:dbname=gis " )
-os.system("python ogr2osm/ogr2osm.py -f -o output/overlappingbuildings.osm temp/overlappingbuildings.shp")
+if ( os.system("ogr2ogr -sql \"" + sql + "\" -overwrite -f 'ESRI Shapefile' " + tempShapeFile + ".shp PG:dbname=\"" + param.database + "\" ") ) :
+  print("error: ogr2ogr command failed.")
+  sys.exit(1)
+
+if ( os.system(ogr2osmCmd + " -f -o " + outputFile + " " + tempShapeFile + ".shp") ) :
+  print("error: ogr2osm command failed.")
+  os.remove(tempShapeFile + ".shp")
+  os.remove(tempShapeFile + ".dbf")
+  os.remove(tempShapeFile + ".shx")
+  sys.exit(1)
+
+os.remove(tempShapeFile + ".shp")
+os.remove(tempShapeFile + ".dbf")
+os.remove(tempShapeFile + ".shx")
+
+print("Writing output file " + outputFile)
+
+sys.exit(0)
 
 
 
-
-  
